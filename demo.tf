a336ba9631f6e51e3e80af32c069617ea5f21aa9
GouseAli <89305721+GouseAli@users.noreply.github.com>
2022-01-18 18:28:37 +0500

    Update demo.tf

--- demo.tf
+++ demo.tf
@@ -5,8 +5,33 @@ provider "aws" {
 }
 terraform {
   backend "s3" {
-    bucket = "[YOUR_REMOTE_STATE_BUCKET_NAME]"
-    key    = "[YOUR_REMOTE_STATE_KEY]"
+    bucket = "[my-remote-bucket-2022]"
+    key    = "[my-remote-bucket-2022]"
     region = "eu-central-1"
+  }
+}
+resource "aws_s3_bucket" "s3Bucket" {
+  bucket = "[my-remote-bucket-2022]"
+  acl    = "public-read"
+
+  policy = <<EOF
+{
+  "Id": "MakePublic",
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Action": [
+        "s3:GetObject"
+      ],
+      "Effect": "Allow",
+      "Resource": "arn:aws:s3:::[my-remote-bucket-2022]/*",
+      "Principal": "*"
+    }
+  ]
+}
+EOF
+
+  website {
+    index_document = "index.html"
   }
 }
