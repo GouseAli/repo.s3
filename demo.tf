82cdde61f8605981f31220689f25cfaa3c57f461
GouseAli <89305721+GouseAli@users.noreply.github.com>
2022-01-18 18:13:46 +0500

    Update demo.tf

--- demo.tf
+++ demo.tf
@@ -3,3 +3,10 @@ provider "aws" {
   version = "~> 2.0"
   region  = "eu-central-1"
 }
+terraform {
+  backend "s3" {
+    bucket = "[YOUR_REMOTE_STATE_BUCKET_NAME]"
+    key    = "[YOUR_REMOTE_STATE_KEY]"
+    region = "eu-central-1"
+  }
+}
